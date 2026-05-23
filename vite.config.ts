3ac61d4b1df4ea724139a449900e0440f13f4376
Hung Le <hung.le.ext@ericsson.com>
2026-05-23 21:51:20 +0700

    Remove index.html file and update Vite configuration to dynamically scan for index.html files in the pages directory, enhancing build input management.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import fs from "node:fs";
+import { resolve, relative, dirname } from "node:path";
+
+function scanIndexHtml(dir: string, base = dir) {
+  const entries: Record<string, string> = {};
+
+  for (const item of fs.readdirSync(dir, { withFileTypes: true })) {
+    const fullPath = resolve(dir, item.name);
+
+    if (item.isDirectory()) {
+      Object.assign(entries, scanIndexHtml(fullPath, base));
+      continue;
+    }
+
+    if (item.isFile() && item.name === "index.html") {
+      const relDir = relative(base, dirname(fullPath));
+      console.log(relDir, fullPath);
+      entries[relDir] = fullPath;
+    }
+  }
+
+  return entries;
+}
 
 export default defineConfig({
-  plugins: [tsconfigPaths()]
-})
+  root: resolve(__dirname, "pages"),
+  build: {
+    rollupOptions: {
+      input: scanIndexHtml(resolve(__dirname, "pages")),
+    },
+  },
+  plugins: [tsconfigPaths()],
+});
